4ce623cd47ad24846e78777bad0644fb71702906
Elteyab Hassan <Mr.elteb@gmail.com>
2022-09-04 21:36:43 +0200

    fix Name Space and move to Adaa

---
+++ src/HijriDatesServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Adaa\LaravelHijri;
+
+
+use GeniusTS\HijriDate\Date;
+use GeniusTS\HijriDate\Hijri;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class HijriDatesServiceProvider
+ *
+ * @package GeniusTS\LaravelHijri
+ */
+class HijriDatesServiceProvider extends ServiceProvider
+{
+
+    protected $configuration = __DIR__ . '/../resources/config/hijri-dates.php';
+
+    protected $translations = __DIR__ . '/../resources/lang';
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([$this->configuration => config_path('hijri-dates.php')], 'config');
+        $this->publishes([$this->translations => resource_path('lang/vendor/hijri-dates/')], 'translation');
+
+        $this->mergeConfigFrom($this->configuration, 'hijri-dates');
+        $this->loadTranslationsFrom($this->translations, 'hijri-dates');
+
+        $translation = Config::get('hijri-dates.translation');
+        Date::setTranslation(new $translation);
+        Date::setToStringFormat(Config::get('hijri-dates.toStringFormat'));
+        Date::setDefaultNumbers(Config::get('hijri-dates.numeric_system'));
+
+        Hijri::setDefaultAdjustment(Config::get('hijri-dates.adjustment'));
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
